bddf547948374f0024c56efaefd11c0fedc8cee5
Vishwesh Patil <vishveshpatil99@gmail.com>
2024-11-18 20:01:09 +0530

    Added DayTime.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -19,42 +19,69 @@ class _HomeState extends State<Home> {
       data = arguments;
     }
 
-    // print(data);
+    //print(data);
+
+    //set background
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    Color bgColor = data['isDaytime'] ? Colors.blue : Colors.indigo[700] ?? Colors.indigo;
 
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-          child: Column(
-            children: <Widget>[
-              ElevatedButton.icon(
-                icon: Icon(Icons.edit_location),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/location');
-                },
-                label: Text('Edit Location'),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'],
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            child: Column(
+              children: <Widget>[
+                ElevatedButton.icon(
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/location');
+                  },
+                  label: Text(
+                    'Edit Location',
                     style: TextStyle(
-                      fontSize: 28,
-                      letterSpacing: 2.0,
+                      color: Colors.grey[300],
                     ),
-                  )
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 55,
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                  ),
                 ),
-              )
-            ],
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 55,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
